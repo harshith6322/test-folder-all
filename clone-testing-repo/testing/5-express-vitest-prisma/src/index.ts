22b6c375cd6650e8f15743169114dc16a1437e50
harshithreddy6322 <harshithreddy6322@gmail.com>
2024-09-03 10:36:05 +0530

    test

--- clone-testing-repo/testing/5-express-vitest-prisma/src/index.ts
+++ clone-testing-repo/testing/5-express-vitest-prisma/src/index.ts
@@ -6,59 +6,58 @@ export const app = express();
 app.use(express.json());
 
 const sumInput = z.object({
-    a: z.number(),
-    b: z.number()
-})
+  a: z.number(),
+  b: z.number(),
+});
 
 app.post("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse(req.body)
-    
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
-    }
+  const parsedResponse = sumInput.safeParse(req.body);
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
 
-    const response = await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
-    })
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
 
-    res.json({
-        answer,
-        id: response.id
-    })
+  const response = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+
+  res.json({
+    answer,
+    id: response.id,
+  });
 });
 
 app.get("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse({
-        a: Number(req.headers["a"]),
-        b: Number(req.headers["b"])
-    })
-    
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
-    }
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
 
-    const response = await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
-    })
-    res.json({
-        answer,
-        id: response.id
-    })
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  const response = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+  res.json({
+    answer,
+    id: response.id,
+  });
 });
-
